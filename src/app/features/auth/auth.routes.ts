3c0e2f9eb436c5b99d9738a48ab81a697d0c859b
Hamidreza Ghasemi <hamid.r.qasemi@gmail.com>
2026-02-05 11:00:29 +0330

    Login page
    feat: added login page with credentials using mock APIs from JSON server.

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './login/login.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const AUTH_ROUTES: Routes = [
   {
---
+++ src/app/features/auth/pages/login/login.page.ts
@@ -0,0 +1,62 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Icon } from '../../../../shared/components/icon/icon';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { Auth } from '../../../../core/auth/auth.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    Icon,
+    FloatLabelModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    ReactiveFormsModule,
+    MessageModule,
+  ],
+  templateUrl: './login.page.html',
+  styleUrl: './login.page.scss',
+})
+export class LoginPage implements OnInit {
+  private auth = inject(Auth);
+  private fb = inject(FormBuilder);
+
+  error = signal<string | null>(null);
+  loading = signal(false);
+
+  form = this.fb.nonNullable.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  ngOnInit(): void {}
+
+  async login() {
+    debugger;
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading.set(true);
+    this.error.set(null);
+
+    try {
+      await this.auth.login(this.form.getRawValue());
+    } catch (e: any) {
+      debugger;
+      this.error.set(e.message);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
+  logit(item: any) {
+    console.log(item);
+  }
+}
